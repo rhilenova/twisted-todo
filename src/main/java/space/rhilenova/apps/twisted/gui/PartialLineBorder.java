6b4cfb69fa76f3875de4e07fd51e8b9a8bf8c813
rhilenova <rhilenova@gmail.com>
2016-03-24 13:08:43 -0400

    Finished PartialLineBorder

--- src/main/java/space/rhilenova/apps/twisted/gui/PartialLineBorder.java
+++ src/main/java/space/rhilenova/apps/twisted/gui/PartialLineBorder.java
@@ -2,12 +2,16 @@ package space.rhilenova.apps.twisted.gui;
 
 import javax.swing.border.AbstractBorder;
 import java.awt.*;
+import java.awt.geom.AffineTransform;
 import java.util.Arrays;
 import java.util.List;
 import org.apache.commons.collections4.CollectionUtils;
 
 /**
- * TODO: Document
+ * A variant of LineBorder, where the sides and corners can be selected individually.
+ *
+ * The specification of which parts to draw is done through the "sides" argument of the constructor. Which number
+ * corresponds to which side is as follows:
  * 0, 1, 2
  * 3,    4
  * 5, 6, 7
@@ -20,9 +24,6 @@ class PartialLineBorder extends AbstractBorder
     private final Color _color;
     /** The width of the lines to draw. */
     private int _thickness;
-
-    /** The divisor to use for calculating corner sizes. */
-    private float _divisor;
     /** The height of the corners. */
     private int _corner_height;
     /** The width of the corners. */
@@ -43,25 +44,6 @@ class PartialLineBorder extends AbstractBorder
      * @param sides A list of sides to draw. See class documentation for more info.
      * @param color The color to draw the lines.
      * @param thickness The width of lines to draw.
-     * @param divisor The divisor to use for calculating corner sizes.
-     */
-    PartialLineBorder(List<Integer> sides, Color color, int thickness, float divisor)
-    {
-        this._sides = sides;
-        this._color = color;
-        this._thickness = thickness;
-
-        this._divisor = divisor;
-        this._corner_height = 0;
-        this._corner_width = 0;
-    }
-
-    /**
-     * A partial border, allowing for edges and corners to be specified individually.
-     *
-     * @param sides A list of sides to draw. See class documentation for more info.
-     * @param color The color to draw the lines.
-     * @param thickness The width of lines to draw.
      * @param corner_height The height of the corners.
      * @param corner_width The width of the corners.
      */
@@ -70,8 +52,6 @@ class PartialLineBorder extends AbstractBorder
         this._sides = sides;
         this._color = color;
         this._thickness = thickness;
-
-        this._divisor = 0;
         this._corner_height = corner_height;
         this._corner_width = corner_width;
     }
@@ -86,86 +66,73 @@ class PartialLineBorder extends AbstractBorder
             g2d.setColor(this._color);
 
             // Get width of line and corners.
-            int corner_width;
-            int line_width;
-            int corner_height;
-            int line_height;
-            if (_divisor != 0)
-            {
-                corner_width = Math.round(c.getWidth() / this._divisor);
-                line_width = c.getWidth() - (2 * corner_width);
-                corner_height = Math.round(c.getHeight() / this._divisor);
-                line_height = c.getHeight() - (2 * corner_height);
-            }
-            else
-            {
-                corner_width = this._corner_width;
-                line_width = c.getWidth() - (2 * corner_width);
-                corner_height = this._corner_height;
-                line_height = c.getHeight() - (2 * corner_height);
-            }
+            int corner_width = this._corner_width;
+            int line_width = c.getWidth() - (2 * corner_width);
+            int corner_height = this._corner_height;
+            int line_height = c.getHeight() - (2 * corner_height);
 
             // TopLeft
             if (this._sides.contains(0))
             {
-                // Horiz
-                g2d.drawRect(0, 0, corner_width, this._thickness - 1);
-                // Vert
-                g2d.drawRect(0, 0, this._thickness - 1, corner_height);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float();
+                path.append(new Rectangle(0, 0, corner_width, this._thickness), false);
+                path.append(new Rectangle(0, 0, this._thickness, corner_height), false);
+                g2d.fill(path);
             }
             // Top
             if (this._sides.contains(1))
             {
-                g2d.drawRect(corner_width, 0,
-                             line_width, this._thickness - 1);
+                Shape rect = new Rectangle(corner_width, 0, line_width, this._thickness);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float(rect);
+                g2d.fill(path);
             }
             // TopRight
             if (this._sides.contains(2))
             {
-                // Horiz
-                g2d.drawRect(corner_width + line_width, 0,
-                             corner_width, this._thickness - 1);
-                // Vert
-                g2d.drawRect(c.getWidth() - this._thickness, 0,
-                             this._thickness - 1, corner_height);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float();
+                path.append(new Rectangle(0, 0, corner_width, this._thickness), false);
+                path.append(new Rectangle(corner_width - this._thickness, 0, this._thickness, corner_height), false);
+                path.transform(AffineTransform.getTranslateInstance(corner_width + line_width, 0f));
+                g2d.fill(path);
             }
             // Left
             if (this._sides.contains(3))
             {
-                g2d.drawRect(0, corner_height,
-                             this._thickness - 1, line_height);
+                Shape rect = new Rectangle(0, corner_height, this._thickness, line_height);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float(rect);
+                g2d.fill(path);
             }
             // Right
             if (this._sides.contains(4))
             {
-                g2d.drawRect(c.getWidth() - this._thickness, corner_height,
-                             this._thickness - 1, line_height);
+                Shape rect = new Rectangle(c.getWidth() - this._thickness, corner_height, this._thickness, line_height);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float(rect);
+                g2d.fill(path);
             }
             // BottomLeft
             if (this._sides.contains(5))
             {
-                // Horiz
-                g2d.drawRect(0, c.getHeight() - this._thickness,
-                             corner_width, this._thickness - 1);
-                // Vert
-                g2d.drawRect(0, corner_height + line_height - 1,
-                             this._thickness - 1, corner_height);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float();
+                path.append(new Rectangle(0, corner_height - this._thickness, corner_width, this._thickness), false);
+                path.append(new Rectangle(0, 0, this._thickness, corner_height), false);
+                path.transform(AffineTransform.getTranslateInstance(0f, corner_height + line_height));
+                g2d.fill(path);
             }
             // Bottom
             if (this._sides.contains(6))
             {
-                g2d.drawRect(corner_width, c.getHeight() - this._thickness,
-                             line_width, this._thickness - 1);
+                Shape rect = new Rectangle(corner_width, c.getHeight() - this._thickness, line_width, this._thickness);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float(rect);
+                g2d.fill(path);
             }
             // BottomRight
             if (this._sides.contains(7))
             {
-                // Horiz
-                g2d.drawRect(corner_width + line_width, c.getHeight() - this._thickness,
-                             corner_width, this._thickness - 1);
-                // Vert
-                g2d.drawRect(c.getWidth() - this._thickness, corner_height + line_height - 1,
-                             this._thickness - 1, corner_height);
+                java.awt.geom.Path2D.Float path = new java.awt.geom.Path2D.Float();
+                path.append(new Rectangle(0, corner_height - this._thickness, corner_width, this._thickness), false);
+                path.append(new Rectangle(corner_width - this._thickness, 0, this._thickness, corner_height), false);
+                path.transform(AffineTransform.getTranslateInstance(corner_width + line_width, corner_height + line_height));
+                g2d.fill(path);
             }
 
             g2d.setColor(old_color);
